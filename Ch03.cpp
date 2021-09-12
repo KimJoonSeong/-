4cdaade303f0441e17da9956fea14bf5bfb94b02
OWNER <OWNER@PC-A>
2021-09-12 22:11:58 +0900

    초기 커밋.

---
+++ Ch03.cpp
@@ -0,0 +1,24 @@
+﻿// Ch03.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+
+#include <stdio.h>
+
+void strcat(const char* s1, const char* s2, char* s3) {
+   
+
+}
+#include <stdio.h>
+
+int main()
+{
+    int* pi = new int;
+    *pi = 10;
+
+    int* pj = new int[2];
+    pj[0] = 20; pj[1] = 30;
+
+    delete[] pi;
+    delete[] pj;
+}
+  
